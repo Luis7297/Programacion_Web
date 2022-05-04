6a19dfe9e11fbc05d1db9dc0733f940e2821dd09
Luis7297 <104519687+Luis7297@users.noreply.github.com>
2022-05-04 10:13:26 -0500

    Hoy vimos Ciclo For

---
+++ Ciclos 2/array-bucles.js
@@ -0,0 +1,38 @@
+// Declaramos un Array
+
+
+var pokemones=["Ditto", "Eeve", "Metapod"];
+
+// Definir nuestra variable i
+
+
+var i=0;
+
+// Ciclo While
+/* 
+while(i<=2){
+    // Código que se ejecuta
+    console.log(pokemones[i]);
+    i++;
+} 
+*/
+
+// Ciclo Do/While
+
+/* 
+do{
+//Código a 
+console.log(pokemones[i]);
+i++;
+} while(i<=2); 
+*/
+
+// Ciclo For
+
+
+for(var i=0; i<=2; i++){
+    // Código que se ejecuta
+// console.log(pokemones[i]);
+document.write(pokemones[i] + " yo te elijo! <br>");
+}
+
